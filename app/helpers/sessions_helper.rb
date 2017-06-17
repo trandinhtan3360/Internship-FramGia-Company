475a77af2860e30cdc4f5162fdb97cc32c9543c4
Tran Dinh Tan <trandinhtan.web@gmail.com>
2017-06-17 22:33:33 +0700

    Add function login and remember me

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,7 @@
-module SessionsHelper   def log_in(user)     session[:user_id] = user.id   end
+module SessionsHelper   
+  def log_in(user)     
+    session[:user_id] = user.id   
+  end
 
   def remember(user)
     user.remember
@@ -11,12 +14,13 @@ module SessionsHelper   def log_in(user)     session[:user_id] = user.id   end
   end
 
   # Returns the current logged-in user (if any).
-  def current_user
-   if (user_id = session[:user_id])
+ def current_user
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
+      raise  
       user = User.find_by(id: user_id)
-       if user && user.authenticated?(:remember, cookies[:remember_token])
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -27,10 +31,10 @@ module SessionsHelper   def log_in(user)     session[:user_id] = user.id   end
     !current_user.nil?
   end
 
-    def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
+  def forget(user)
+  user.forget
+  cookies.delete(:user_id)
+  cookies.delete(:remember_token)
   end
   
   def log_out
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,8 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
