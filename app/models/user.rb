e3eddef9496118b03690ce6b6e5112e642dfc863
Tran Dinh Tan <trandinhtan.web@gmail.com>
2017-06-20 08:51:09 +0700

    add static page

---
+++ app/models/user.rb
@@ -0,0 +1,79 @@
+class User < ApplicationRecord
+  has_many :microposts
+	has_many :microposts,  dependent: :destroy
+  
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  
+  before_save { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  class << self
+    def digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+    end
+
+  # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    remember_token = new_token
+    update_attribute(:remember_digest, digest(remember_token))
+  end
+
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+private
+# Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+  end
+
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'static_pages/404'
+  get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
